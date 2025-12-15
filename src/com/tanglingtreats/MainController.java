e06749d20284356e706563d7017ca47ce3ba7c0e
TanglingTreats <foolstacked@gmail.com>
2025-12-16 00:15:34 +0800

    feat(javafx): Add dropdown box to select format

--- src/com/tanglingtreats/MainController.java
+++ src/com/tanglingtreats/MainController.java
@@ -1,13 +1,40 @@
 package com.tanglingtreats;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Accordion;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
     @FXML
     TextArea outputBox;
 
     @FXML
     TextArea inputBox;
+
+    @FXML
+    ChoiceBox<String> formatChoices;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ObservableList<String> formats = FXCollections.observableArrayList(
+          "None"
+        );
+
+        formatChoices.setItems(formats);
+        formatChoices.getSelectionModel().selectFirst();
+    }
+
+    @FXML
+    private void handleFormatAction() {
+        String selectedFormat = formatChoices.getSelectionModel().getSelectedItem();
+
+        System.out.println("Selected format: " + selectedFormat);
+    }
 }
